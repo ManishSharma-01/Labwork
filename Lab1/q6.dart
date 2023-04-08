c34140565943af953f542ac8e9ba5c3333e08b2c
ManishSharma-01 <27.maanish@gmail.com>
2023-04-08 13:38:20 +0545

    extra

--- Lab1/q6.dart
+++ Lab1/q6.dart
@@ -1,7 +1,15 @@
 //multiplication tables of 5
-
 import 'dart:io';
 
-table(){
-  stdout.write("Enter the ")
+multiply() {
+  stdout.write("Enter the number of table up to which you want: ");
+  int num = int.parse(stdin.readLineSync()!);
+
+  for (int i = 1; i <= num; i++) {
+    print("5 * $i = ${i * 5}");
+  }
+}
+
+void main(List<String> args) {
+  multiply();
 }
--- Lab1/q7.dart
+++ Lab1/q7.dart
@@ -1,1 +1,16 @@
 //multiplication tables of 1-9
+
+multiply() {
+  int num = 10;
+
+  for (int i = 1; i <= num; i++) {
+    for (int j = 1; j <= num; j++) {
+      print("$i * $j = ${j * 1}");
+    }
+    // print("5 * $i = ${i * 5}");
+  }
+}
+
+void main(List<String> args) {
+  multiply();
+}
